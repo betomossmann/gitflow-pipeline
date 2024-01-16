70d28bcc62973c3300d7903b81277a55b7bf74a3
Gilberto Mossmann <65679707+betomossmann@users.noreply.github.com>
2024-01-16 14:25:44 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({
+    status: 200,
+    message: 'Hello World!'
+  })
+})
+
+app.listen(8080, () => {
+  console.log('Server listening on port 8080')
+})
